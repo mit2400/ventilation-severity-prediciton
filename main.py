4033d7e37175e7951db016afaff1d52d6ddde23c
minkoo <mit2400@naver.com>
2023-06-12 16:37:56 +0900

    implement multivalidation callback

--- main.py
+++ main.py
@@ -5,8 +5,9 @@
 # from utils.utils import get_args
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # do not show tensorflow INFO log
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # do not show tensorflow INFO and WARNING log
 
+import time
 import tensorflow as tf
 import random
 import numpy as np
@@ -15,10 +16,10 @@ import pickle
 from models.model import MVModel
 from eval.evaluation import eval_severity_scores
 
-# from tensorflow.keras.models import Model, Sequential
-# from tensorflow.keras.layers import LSTM, Bidirectional, Concatenate, GRU
-# from tensorflow.keras.layers import Input, Dense, Dropout
-# from tensorflow.keras.layers import BatchNormalization, GlobalMaxPooling1D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import LSTM, Bidirectional, Concatenate, GRU
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.layers import BatchNormalization, GlobalMaxPooling1D
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras import metrics, regularizers
 from sklearn.utils import class_weight
@@ -33,13 +34,6 @@ def set_seed(seed=42):
 def main():
     set_seed()
     
-    config = []
-    # try:
-    #     args = get_args()
-    #     config = process_config(args.config)
-    # except:
-    #     print("missing or invalid arguments")
-    #     exit(0)
     config = {}
     config['device'] = 'GPU:0'
     config['n_units'] = 16 # in lstm layer
@@ -49,125 +43,109 @@ def main():
     config['activation'] = 'sigmoid'
     config['regularizer'] = 0.01
 
+    # load data
+    test = True
+    if test:
+        with open("data/X_sample.pkl", "rb") as f:            X = pickle.load(f)
+        with open("data/Y_sample.pkl", "rb") as f:            Y = pickle.load(f)
+    else:
+        with open("data/X.pkl", "rb") as f:            X = pickle.load(f)
+        with open("data/Y.pkl", "rb") as f:            Y = pickle.load(f)
+    print("Train shape: {}, {}, Valid shape: {}, {}".format(X[0].shape, Y[0].shape, X[1].shape, Y[1].shape))
+    
+    batch_size = 128
+    train_dataset = tf.data.Dataset.from_tensor_slices((X[0], Y[0]))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+    val_dataset1 = tf.data.Dataset.from_tensor_slices((X[1], Y[1]))
+    val_dataset1 = val_dataset1.batch(batch_size)
+    val_dataset2 = tf.data.Dataset.from_tensor_slices((X[2], Y[2]))
+    val_dataset2 = val_dataset2.batch(batch_size)
+    val_dataset3 = tf.data.Dataset.from_tensor_slices((X[3], Y[3]))
+    val_dataset3 = val_dataset3.batch(batch_size)
+    class_weights = class_weight.compute_class_weight(class_weight ='balanced', classes=np.unique(Y[0]), y =Y[0])
+    print(f'input shape: {X[0].shape[-2:]}')
+
+
+    def get_uncompiled_model(drop_rate=0.4, num_units=16, reg = 0.01):
+        x_input = Input(shape=(X[0].shape[-2:])) # (6, 32)
+        d0 = Dropout(drop_rate)(x_input)
+        x1 = Bidirectional(LSTM(units=num_units, return_sequences=True))(d0)
+        d1 = Dropout(drop_rate)(x1)
+        l1 = Bidirectional(LSTM(units=num_units, return_sequences=True))(d1)
+        d2 = Dropout(drop_rate)(l1)
+        l2 = Bidirectional(LSTM(units=num_units, return_sequences=False))(d2)
+        d3 = Dropout(drop_rate)(l2)
+        d4 = Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(reg))(d3)
+        model = Model(inputs=x_input, outputs=d4, name='Bidirectional_LSTM')
+        return model
+    
+    def get_compiled_model(drop_rate=0.4, num_units=16, reg = 0.01, learning_rate=1e-3,label_smoothing=0.2):
+        model = get_uncompiled_model(drop_rate, num_units, reg)
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate), 
+            loss=tf.keras.losses.BinaryCrossentropy(label_smoothing=label_smoothing),
+            metrics=[metrics.AUC(name='auc', curve='ROC'), metrics.AUC(name='ap', curve='PR')]   
+        )
+        return model
+
+    #todo
     dp = 0.4
     nunit = 16
     lr=1e-3
     ls=0.2
-    epoch=20
+    epochs=3
 
-    # # create the experiments dirs
-    # create_dirs([config.summary_dir, config.checkpoint_dir])
-    checkpoint_filepath = f"./result/run_{dp}_{nunit}_{lr}.hdf5"
+    lr_sched = tf.keras.optimizers.schedules.CosineDecay(1e-2,epochs)
+    # lr_sched = ReduceLROnPlateau(monitor='val_auc', mode='max', factor=0.3, min_lr=1e-6, patience=5, verbose=True)
+    model = get_compiled_model(learning_rate = lr_sched )
+    model.summary() 
     
-    # load data
-    with open("data/X.pkl", "rb") as f:
-        X = pickle.load(f)
-    with open("data/Y.pkl", "rb") as f:
-        Y = pickle.load(f)
-    
-    # with open("data/X_sample.pkl", "rb") as f:
-    #     X = pickle.load(f)
-    # with open("data/Y_sample.pkl", "rb") as f:
-    #     Y = pickle.load(f)
-
-    eval_severity_scores(X,Y)
-    class_weights = class_weight.compute_class_weight(class_weight ='balanced', classes=np.unique(Y[0]), y =Y[0])
-
-    model = MVModel(config)
-    print(model(X[3][:1]))
-
-
-
-
-    # Instantiate an optimizer.
-    optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
-    # Instantiate a loss function.
-    loss_fn = tf.keras.losses.BinaryCrossentropy(label_smoothing=ls)
-
-    batch_size = 128
-    train_dataset = tf.data.Dataset.from_tensor_slices((X[0], Y[0]))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-
-    
-    # Prepare the validation dataset.
-    val_dataset = tf.data.Dataset.from_tensor_slices((X[1], Y[1]))
-    val_dataset = val_dataset.batch(batch_size)
-
-    epochs = 2
-    for epoch in range(epochs):
-        print("\nStart of epoch %d" % (epoch,))
-        # https://www.tensorflow.org/guide/keras/writing_a_training_loop_from_scratch/?hl=ko
-
-        # Iterate over the batches of the dataset.
-        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-
-            # Open a GradientTape to record the operations run
-            # during the forward pass, which enables auto-differentiation.
-            with tf.GradientTape() as tape:
-
-                # Run the forward pass of the layer.
-                # The operations that the layer applies
-                # to its inputs are going to be recorded
-                # on the GradientTape.
-                logits = model(x_batch_train, training=True)  # Logits for this minibatch
-
-                # Compute the loss value for this minibatch.
-                loss_value = loss_fn(y_batch_train, logits)
-
-            # Use the gradient tape to automatically retrieve
-            # the gradients of the trainable variables with respect to the loss.
-            grads = tape.gradient(loss_value, model.trainable_weights)
-
-            # Run one step of gradient descent by updating
-            # the value of the variables to minimize the loss.
-            optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-            # Log every 200 batches.
-            if step % 200 == 0:
-                print(
-                    "Training loss (for one batch) at step %d: %.4f"
-                    % (step, float(loss_value))
-                )
-                print("Seen so far: %s samples" % ((step + 1) * batch_size))
-
-
-
-
-
-    
-    # model.compile(
-    #     optimizer=tf.keras.optimizers.Adam(learning_rate=lr), 
-    #     loss=tf.keras.losses.BinaryCrossentropy(label_smoothing=ls), 
-    #     metrics=[metrics.AUC(name = 'auc')])
-
-    # lr = ReduceLROnPlateau(monitor='val_auc', mode='max', factor=0.3, min_lr=1e-6, patience=5, verbose=True)
-    # #es = EarlyStopping(monitor='val_auc', mode='max', patience=10, restore_best_weights=True, verbose=True)
+    checkpoint_filepath = f"./result/Model_{dp}_{nunit}_{lr}"
+    sv = tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(checkpoint_filepath,"run_{epoch}"),
+        save_best_only=False,save_weights_only=False,monitor="val_auc",mode='max',verbose=1, options=None
+    )
     # sv = tf.keras.callbacks.ModelCheckpoint(
     #     checkpoint_filepath, monitor='val_auc', verbose=1, save_best_only=True,
-    #     save_weights_only=True, mode='max', save_freq='epoch', options=None
+    #     save_weights_only=False, mode='max', save_freq='epoch', options=None
     # )
     
-    # print("Train shape: {}, {}, Valid shape: {}, {}".format(X[0].shape, Y[0].shape, X[1].shape, Y[1].shape))
+    tb = tf.keras.callbacks.TensorBoard(log_dir=f"./summaries/Model_{dp}_{nunit}_{lr}") 
     
+    class MultiValidationCallback(tf.keras.callbacks.Callback):
+        def __init__(self, val_datasets):
+            super().__init__()
+            self.val_datasets = val_datasets
+
+        def on_epoch_end(self, epoch, logs=None):
+            lr = self.model.optimizer.lr
+            # If the learning rate is a decayed learning rate, we need to compute it
+            if isinstance(lr, tf.keras.optimizers.schedules.LearningRateSchedule):
+                lr = lr(epoch)
+            print(f'learning rate: {lr}')
+            
+            metrics = [[] for _ in range(3)]
+            for i, val_dataset in enumerate(self.val_datasets):
+                val_loss, val_auc, val_ap = self.model.evaluate(val_dataset)
+                metrics[0].append(val_loss)
+                metrics[1].append(val_auc)
+                metrics[2].append(val_ap)
+            for i in range(len(metrics[0])):
+                print(f"Val_{i}, loss: {metrics[0][i]:.3f}, AUC: {metrics[1][i]:.3f}, AP: {metrics[2][i]:.3f}")
+    mv = MultiValidationCallback([val_dataset2, val_dataset3])
+
+    hist=model.fit(train_dataset, validation_data=val_dataset1, class_weight=dict(enumerate(class_weights)), 
+            epochs=epochs, batch_size=128, callbacks=[sv,mv,tb], verbose=True)
     # hist=model.fit(X[0], Y[0], validation_data=(X[1], Y[1]), class_weight=dict(enumerate(class_weights)), 
-    #         epochs=epoch, batch_size=128, callbacks=[lr,sv], verbose=True)
+    #         epochs=epochs, batch_size=128, callbacks=[lr,sv], verbose=True)
+    # hist=model.fit(train_dataset, validation_data=val_dataset, class_weight=dict(enumerate(class_weights)), 
+    #         epochs=epochs, batch_size=128, callbacks=[lr,sv], verbose=True)
 
-    
-
-    # for i in range (1,4):
-    #     model.evaluate(X[i], Y[i], verbose=2)
-
-    
-
-    # y_valid=[]
-    # y_prob=[]
-    # for i in range(1,4):
-    #     y_prob.append(model.predict(X[i]).squeeze())  
-    #     y_valid.append(Y[i])
-
-    # # create tensorboard logger
-    # logger = Logger(sess, config)
-
+    y_valid=[]
+    y_prob=[]
+    for i in range(1,4):
+        y_prob.append(model.predict(X[i]).squeeze())  
+        y_valid.append(Y[i])
 
 if __name__ == '__main__':
     print("TensorFlow version:", tf.__version__)
